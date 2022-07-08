d9d59dd9103e1c01808c36c4d5de62f12e693e06
Jim Clark <jim@atomist.com>
2022-07-08 10:17:41 -0700

    Webhook schema change

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ api.start(async payload => {
       [{"schema/entity-type": new api.Keyword("vonwig.testing/observation"),
         "vonwig.testing.observation/id": uuidv4().toString(),
         "vonwig.testing.observation/seen-by-subscriber": false,
-        "vonwig.testing.observation/webhook-value": payload.context.webhook.body}])
+        "vonwig.testing.observation/webhook-value": payload.context.webhook.request.body}])
       .then(_ => {return {state: "completed", reason: "created observation"}})
-  } else if ("subscription" == payload.type && "on_observation.edn" === payload.context.subscription.name) {
+  } else if ("subscription" == payload.type && "on_observation" === payload.context.subscription.name) {
     return payload.transact(
       [{"schema/entity-type": new api.Keyword("vonwig.testing/observation"),
         "vonwig.testing.observation/id": payload.context.subscription.result[0][0].id,
         "vonwig.testing.observation/seen-by-subscriber": true}])
       .then(_ => {return {state: "completed", reason: "update observation"}})
   } else {
-    return {state: "failed", reason: "unrecognized"}
+    return Promise.resolve({state: "failed", reason: "unrecognized"})
   }
 });
